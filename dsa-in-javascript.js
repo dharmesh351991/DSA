5e71e5a8e3e231fd38e77482eea1b6ef46fd8eb5
Dharmesh Panchal <dharmesh351991@gmail.com>
2023-10-29 15:04:31 +0530

    [Update] - Added new problems

--- dsa-in-javascript.js
+++ dsa-in-javascript.js
@@ -100,7 +100,11 @@ var fizzBuzz = function (n) {
     }
     return myResult;
 };
-
+//Leetcode : 704. Binary Search
+/*Given an array of integers nums which is sorted in ascending order, and an integer target, 
+write a function to search target in nums. 
+If target exists, then return its index. Otherwise, return -1.
+You must write an algorithm with O(log n) runtime complexity.*/
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -351,6 +355,77 @@ const convertDecimalToBinary = (n) => {
     binaryArr = binaryArr.reverse();
     return binaryArr;
 }
+//Leetcode 33. Search in Rotated Sorted Array
+/*
+There is an integer array nums sorted in ascending order (with distinct values).
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) 
+such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). 
+For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+Given the array nums after the possible rotation and an integer target, r
+eturn the index of target if it is in nums, or -1 if it is not in nums.
+You must write an algorithm with O(log n) runtime complexity.
+Example 1:
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+*/
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    var low = 0;
+    var high = nums.length - 1;
+    while(low<=high){
+        var mid = Math.round( ( low + high ) / 2 );
+        if( target == nums[mid] ){
+            return mid;
+        }
+        else if( nums[low] <= nums[mid] ){
+            if( nums[low] <= target && target < nums[mid]){
+                high = mid -1;
+            }else{
+                low = mid + 1;
+            }
+        }else{
+            if( nums[mid] < target && target <= nums[high] ){
+                low = mid+1;
+            }else{
+                high = mid -1;
+            }
+        }
+    }
+    return -1;
+};
+/** 
+ * Lettcode - 153. Find Minimum in Rotated Sorted Array
+*/
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    var low = 0;
+    var high = nums.length-1;
+    var ans = Infinity;
+    while( low <= high){
+        var mid = Math.floor( (low + high) / 2 );
+        if( nums[low] <= nums[high] ){
+            ans = Math.min(ans, nums[low]);
+            break;
+        }
+        //left part is sorted
+        if( nums[low] <= nums[mid] ){
+            ans = Math.min( ans, nums[low] );
+            low = mid + 1;
+        }else{
+            //if right is sorted
+            ans = Math.min(ans, nums[mid]);
+            high = mid - 1;
+        }
+    }
+    return ans;
+};
 console.log('Result', convertDecimalToBinary(19));
 //HW
 
